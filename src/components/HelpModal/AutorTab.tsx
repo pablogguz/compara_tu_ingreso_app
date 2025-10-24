f783786f8866032cae9087a9cb98bc7ab6f7b47d
pablogguz <72149928+pablogguz@users.noreply.github.com>
2025-10-24 23:10:37 +0200

    Update AutorTab.tsx

--- src/components/HelpModal/AutorTab.tsx
+++ src/components/HelpModal/AutorTab.tsx
@@ -3,7 +3,7 @@ export default function AutorTab() {
     <div className="help-content">
       <h4>Sobre el autor</h4>
       <p>
-        Soy Pablo y me dedico a la investigación en economía aplicada. En concreto, trabajo analizando grandes bases de datos administrativas, geoespaciales y encuestas a hogares para entender mejor la sociedad. En mi tiempo libre, me gusta desarrollar proyectos de código abierto y herramientas como esta para hacer los datos más accesibles y útiles para todos.
+        ¡Hola! Soy Pablo y me dedico a la investigación en economía aplicada. En concreto, trabajo analizando grandes bases de datos administrativas, geoespaciales y encuestas a hogares para entender mejor la sociedad. En mi tiempo libre, me gusta desarrollar proyectos de código abierto y herramientas como esta para hacer los datos más accesibles y útiles para todos.
       </p>
 
       <div className="author-social-links">
